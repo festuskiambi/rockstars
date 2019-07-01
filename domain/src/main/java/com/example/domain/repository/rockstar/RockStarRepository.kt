eb83a2a3d7a46e27718240c0279437779073d658
festus <kiamfestoh@gmail.com>
2019-07-01 13:51:11 +0300

    .gitignore fix

--- domain/src/main/java/com/example/domain/repository/rockstar/RockStarRepository.kt
+++ domain/src/main/java/com/example/domain/repository/rockstar/RockStarRepository.kt
@@ -1,7 +1,13 @@
 package com.example.domain.repository.rockstar
 
+import com.example.domain.entity.Entity
+import com.example.domain.common.Result
+import java.lang.Exception
+
 /**
  * Created by Festus Kiambi on 7/1/19.
  */
 interface RockStarRepository {
+
+    fun getRockStars(): Result<Exception, List<Entity.RockStar>>
 }
