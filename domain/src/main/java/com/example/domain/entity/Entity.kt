91ee325f2a549411265e300449f854e5374a7e1b
festus <kiamfestoh@gmail.com>
2019-07-01 13:30:34 +0300

    create rockstar entity and result wrapper

---
+++ domain/src/main/java/com/example/domain/entity/Entity.kt
@@ -0,0 +1,15 @@
+package com.example.domain.entity
+
+/**
+ * Created by Festus Kiambi on 7/1/19.
+ */
+sealed class Entity {
+
+    data class RockStar(
+        val id: String,
+        val index: Int,
+        val name: String,
+        val picture: String,
+        val about: String
+    ) : Entity()
+}
