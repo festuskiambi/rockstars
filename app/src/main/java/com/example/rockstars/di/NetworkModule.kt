4369df062f0eb66dfe839c19dacb14719ccc0dcf
festus <kiamfestoh@gmail.com>
2019-07-02 21:45:21 +0300

    add home view model

--- app/src/main/java/com/example/rockstars/di/NetworkModule.kt
+++ app/src/main/java/com/example/rockstars/di/NetworkModule.kt
@@ -11,6 +11,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
  */
 
 val networkModule = module {
+
     factory { OkHttpClient.Builder().addInterceptor(get()).build() }
 
     single {
---
+++ app/src/main/java/com/example/rockstars/home/viewmodel/HomeViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.rockstars.home.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.domain.common.Result
+import com.example.domain.entity.Entity
+import com.example.domain.usecase.rockstars.IRemoteRockStarUseCase
+import com.example.rockstars.R
+import com.example.rockstars.base.AppDispatchers
+import com.example.rockstars.common.BaseViewModel
+import com.example.rockstars.common.Event
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+/**
+ * Created by Festus Kiambi on 7/2/19.
+ */
+class HomeViewModel(
+    private val iRemoteRockStarUseCase: IRemoteRockStarUseCase,
+    private val dispatchers: AppDispatchers
+) : BaseViewModel() {
+
+    private val rockStarsListState = MutableLiveData< List<Entity.RockStar>>()
+    val rockStarsList: LiveData< List<Entity.RockStar>> get() = rockStarsListState
+
+    init {
+        getRockStars()
+    }
+
+    private fun getRockStars() = viewModelScope.launch(dispatchers.main) {
+
+        withContext(dispatchers.io) {
+
+            val rockStarResult = iRemoteRockStarUseCase.getRockStars()
+
+            when(rockStarResult){
+                is Result.Value -> rockStarsListState.value = rockStarResult.value
+                is Result.Error -> _snackbarError.value =  Event(R.string.error)
+            }
+        }
+
+    }
+}
