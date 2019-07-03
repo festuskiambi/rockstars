10ac672a2390e0d2b0be72eb0ff68585eae6b1ac
festus <kiamfestoh@gmail.com>
2019-07-03 09:49:45 +0300

    create R oom entity

---
+++ data/src/main/java/com/example/data/datamodels/RoomRockstar.kt
@@ -0,0 +1,33 @@
+package com.example.data.datamodels
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+/**
+ * Created by Festus Kiambi on 7/3/19.
+ */
+
+@Entity(
+    tableName = "bookmarks",
+    indices = [Index("id")]
+)
+data class RoomRockstar(
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: String,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "about")
+    val about: String,
+
+    @ColumnInfo(name = "index")
+    val index: Int,
+
+    @ColumnInfo(name = "picture")
+    val picture: String
+)
